8e7378963fc621a53b334195d10c08e8d43519f5
wangbo <pmb2020@163.com>
2020-03-21 13:23:20 +0800

    '更新路由'

--- routes/web.php
+++ routes/web.php
@@ -29,20 +29,20 @@ Route::get('/dbtest','IndexController@dbtest');
 Route::get('/model/test','IndexController@modeltest');
 
 //下面为博客所用路由
-Route::get('/admin/login','admin\LoginController@login');
-Route::get('/admin/loginOut','admin\LoginController@loginOut');
+Route::get('/admin/login','Admin\LoginController@login');
+Route::post('/admin/checkLogin','admin\LoginController@checkLogin');
 Route::get('/admin/getChildCategory/{pid}','admin\CategoryController@getChildCategory');
+
 Route::prefix('admin')->middleware(['age'])->group(function (){
     Route::get('/index','admin\IndexController@index');
     Route::get('/article/index','admin\ArticleController@index');
+    Route::get('/loginOut','admin\LoginController@loginOut');
     Route::post('/article/coverUpload','admin\ArticleController@coverUpload');
     Route::post('/article/imagesUpload','admin\ArticleController@imagesUpload');
+    Route::any('/article/create','admin\ArticleController@create');
+    Route::any('/article/delete/{id}','admin\ArticleController@delete');
 });
 
-Route::post('/admin/checkLogin','admin\LoginController@checkLogin');
-Route::any('/admin/article/create','admin\ArticleController@create');
-Route::any('/admin/article/test','admin\ArticleController@test');
-Route::any('/admin/article/delete/{id}','admin\ArticleController@delete');
 
 //博客前台路由
 Route::get('/','index\IndexController@index');
