67314ee846be59685bf02e3b0cc38e1ed869e7d8
wangbo <pmb2020@163.com>
2020-04-13 00:41:48 +0800

    'seo更新'

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -15,46 +15,46 @@ class CategoriesTableSeeder extends Seeder
         $data=[
             'name' => '爱技术',
             'slug' => '/ajishu',
-            'keywords' => '爱技术,K先生,个人博客',
-            'description' => '分享个人学习经验和技术，有你想要的各种技术干货 - K先生',
+            'keywords' => '爱技术,PHP,laravel,mysql,linux',
+            'description' => '分享自己的学习经验和技术，这里有你想要的各种技术干货',
         ];
         $data1=[
             'name' => '爱分享',
             'slug' => '/ashare',
-            'keywords' => '爱分享,K先生,个人博客',
-            'description' => '分享html模板、js特效、php源码等资源 - K先生',
+            'keywords' => '爱分享,html模板,php源码,个人网站模板,js特效',
+            'description' => '不只是分享html模板、js特效、php源码、个人网站模板等资源，还有更多你想不到的！',
         ];
         $data2=[
             'name' => '爱生活',
             'slug' => '/alife',
-            'keywords' => '爱生活,K先生,个人博客',
-            'description' => '记录生活的点点滴滴，也是心灵的橱窗 - K先生',
+            'keywords' => '爱生活,个人日记,生活资讯,经典语录',
+            'description' => '爱自己就是要爱生活，记录生活的点滴，留住生活的美好！',
         ];
         $data3=[
             'name' => '爱拼搏',
             'slug' => '/apinbo',
-            'keywords' => '爱拼搏,K先生,个人博客',
-            'description' => '只有已经做到的才是真的，一步一个脚印，用脚印记录汗水 - K先生',
+            'keywords' => '爱拼搏,努力奋斗,脚踏实地,进步',
+            'description' => '结果往往要比过程更重要，只有已经做到的才是真的，一步一个脚印，用脚印记录汗水，想要成功，就要爱拼搏！',
         ];
         $data4=[
             'name' => 'html模板',
             'pid'=>2,
             'slug' => '/ashare/html',
-            'keywords' => 'html模板,爱分享,K先生,个人博客',
-            'description' => '为你分享最好看的html模板，不管是个人博客模板，还是企业官网模板，这里都有哦',
+            'keywords' => 'html模板,爱分享,个人网站模板,个人博客模板,企业网站模板',
+            'description' => '为你分享最好看的html模板，你想要的各种模板，理论上这里都有哦！',
         ];
         $data5=[
             'name' => 'js特效',
             'pid'=>2,
             'slug' => '/ashare/js',
-            'keywords' => 'js特效,jquery特效,爱分享,K先生,个人博客',
+            'keywords' => 'js特效,jquery特效,网页特效代码,爱分享',
             'description' => '这里有各种js特效、jquery特效,让你一看就懂的代码',
         ];
         $data6=[
             'name' => '其他',
             'pid'=>2,
             'slug' => '/ashare/other',
-            'keywords' => '爱分享,其他资源,K先生,个人博客',
+            'keywords' => 'php源码,绿色软件,精品软件,黑科技,爱分享',
             'description' => '其实这里才是真正的宝藏，这里有站长各种私藏的秘密资源，只有你想不到的，不来看看？嗯哼？',
         ];
         Category::create($data);
--- database/seeds/ConfigsTableSeeder.php
+++ database/seeds/ConfigsTableSeeder.php
@@ -14,9 +14,9 @@ class ConfigsTableSeeder extends Seeder
         //
         $time=date('Y-m-d H:i:s',time());
         $configs=[
-            ['name'=>'web_title','title'=>'网站标题','value'=>'K先生 - 是一个程序员，也是一个精致boy','created_at'=>$time,'updated_at'=>$time],
-            ['name'=>'web_keywords','title'=>'关键词','value'=>'K先生,K先生个人博客,个人博客,技术干货','created_at'=>$time,'updated_at'=>$time],
-            ['name'=>'web_desc','title'=>'描述','value'=>'K先生个人博客，分享html模板、php源码、技术干货、黑科技等等，总之，这里有很多你想象不到的资源。K先生，是一个程序员，也是一个精致boy','created_at'=>$time,'updated_at'=>$time],
+            ['name'=>'web_title','title'=>'网站标题','value'=>'K先生个人博客 - 分享程序员学习经验和网络资源的个人博客网站','created_at'=>$time,'updated_at'=>$time],
+            ['name'=>'web_keywords','title'=>'关键词','value'=>'K先生,个人博客,个人网站,个人网站模板','created_at'=>$time,'updated_at'=>$time],
+            ['name'=>'web_desc','title'=>'描述','value'=>'K先生个人博客，是一个记录博主学习和成长的个人博客，也是一个分享html模板、php源码、个人网站模板、黑科技等资源的个人网站。','created_at'=>$time,'updated_at'=>$time],
             ['name'=>'web_author','title'=>'作者','value'=>'K先生','created_at'=>$time,'updated_at'=>$time],
             ['name'=>'web_profile','title'=>'简介','value'=>'一个95后程序员，17年入行，一直潜心研究web前端技术，一边工作一边积累经验，分享一些个人博客模板，以及SEO优化等心得。','created_at'=>$time,'updated_at'=>$time],
             ['name'=>'web_about','title'=>'关于我','value'=>'','created_at'=>$time,'updated_at'=>$time],
