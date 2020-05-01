eef213df24c6dcb4ea5fdb9ab2202a4a22ece941
wangbo <pmb2020@163.com>
2020-05-01 19:38:01 +0800

    '0'

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\MOdels\Category;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
