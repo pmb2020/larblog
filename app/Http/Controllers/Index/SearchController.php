f95baa5a6ecce55ef86aa3d617f32be7c553aed8
wangbo <pmb2020@163.com>
2020-05-23 17:08:19 +0800

    '搜索优化'

--- app/Http/Controllers/Index/SearchController.php
+++ app/Http/Controllers/Index/SearchController.php
@@ -12,11 +12,14 @@ class SearchController extends Controller
     //
     public function index(Request $request){
         $keyword=$request->input('keyword');
-//        $results=DB::select('select * from ml_articles where id=?',[1]);
+        if ($keyword){
+        $keywordSql = '%'.implode('%',preg_split("//u", $keyword, -1, PREG_SPLIT_NO_EMPTY)).'%';
+//        dd($keywordSql);
 //        $results=DB::table('articles')->where('title','like',$keywords)->get();
-        $results=Article::where('title','like','%'.$keyword.'%')->with('Category:id,name')->orderBy('id','desc')->paginate(10);
+        $results=Article::where('title','like',$keywordSql)->with('Category:id,name')->orderBy('id','desc')->paginate(10);
+        }
         return view('index.search',[
-            'articles'=>$results,
+            'articles'=>$results??array(),
             'keyword'=>$keyword
         ]);
     }
