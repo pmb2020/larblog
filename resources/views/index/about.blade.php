11cb0303b3c899768c73be66945de0bb1c1cef25
wangbo <pmb2020@163.com>
2021-04-11 18:45:03 +0800

    '更新关于我'

--- resources/views/index/about.blade.php
+++ resources/views/index/about.blade.php
@@ -10,26 +10,13 @@
                 <p style="color: #666;">当前位置：<a href="#">首页</a> > <a href="#">关于我</a></p>
             </div>
             <div class="content">
-                <h2 class="line_h2">关于我———K先生</h2>
-                <div class="user_info">
-                    <span><i id="user_ico" class="ico"></i>林中鸟</span>
-                    <time><i id="time_ico" class="ico"></i>2019-10-10</time>
-                    <span><i id="type_ico" class="ico"></i>爱生活</span><span><i id="read_ico" class="ico"></i>238</span>
-                </div>
+                <h2 class="line_h2">关于我</h2>
                 <div class="blog_text">
-                    <p>我，一个95后草根站长！17年入行。一直潜心研究web前端技术，一边工作一边积累经验，分享一些个人博客模板，以及SEO优化等心得</p>
-                    <p>我，只是一个普通人。</p>
-                    <p>把孤独当作晚餐却难以下咽</p>
-                    <p>把黑夜当作温暖却难以入眠</p>
-                    <p>只好对自己说晚安</p>
-                    <p>无人在身边</p>
-                    <p>想要遗忘却不心甘</p>
-                    <p>把难过当作幻念都烟消云散</p>
-                    <p>把感情当作红线全部都斩断</p>
-                    <p>这时间好像都放慢</p>
-                    <p>像度日如年</p>
-                    <p>想要遗忘我还是不甘</p>
-                    <p>......</p>
+                    <p><strong>网名：</strong>K先生</p>
+                    <p><strong>职业：</strong>全栈开发工程师</p>
+                    <p><strong>技能：</strong>PHP语言启蒙，熟练掌握Thinkphp和laravel框架，对前端技术也有着一定的研究，如前端的vue框架。</p>
+                    <p><strong>我擅长的：</strong>PHP网站开发、小程序开发、H5 app软件开发、电脑端exe程序开发（vue）等等。</p>
+                    <p><strong>简介：</strong>我，渴望大海！</p>
                 </div>
                 <div class="" style="text-align: center;margin: 10px;">
                     <button type="button" style="background-color: red;border: none;padding: 5px 10px;color: #fff;">👍赞一下（57）</button>
